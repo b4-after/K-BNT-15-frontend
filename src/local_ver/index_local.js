f9dc1ea58d9a3c426ab256c43ebab4fe89b173d6
taehwankim2017 <130199355+taehwankim2017@users.noreply.github.com>
2023-05-17 15:25:30 +0900

    현재 https 이외에서 mediarecorder 차단으로 인해 로컬로 돌리는 파일입니다. 생각보다 큰 차이는 없습니다. 파일 경로와, 클릭 추적 기능 정도 입니다. 그 기능은 로컬로 돌리니 안 돌아가는 걸 넘어 로딩 시 렉이 걸리더라고요...
    최신 수정 사항은 아래와 같습니다.
    
    로컬 파일로 실행한다는 전제 하에,
    이제 index 페이지도 test 페이지와 연동되며, 서버로 나이 전송 및 member_Id 받아와 localStorage 에 저장,  test 에서 그걸 이용하는 것이 구현되었습니다.
    또한, 추가로 타임아웃 기능도 구현했습니다. 이제 시간 15초가 경과되면 잔존 시간 0으로 표시가 되자마자 페이지 전환, 이후 alert 창이 뜹니다.

---
+++ src/local_ver/index_local.js
@@ -0,0 +1,60 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const ageSelect = document.getElementById("age-select");
+
+    for (let i = 55; i <= 94; i++) {
+        const option = {
+            value: i,
+            text: i + "세"
+        };
+        ageSelect.add(new Option(option.text, option.value));
+    }
+
+    /*버튼 클릭 시*/
+    document.getElementById("start-button").addEventListener("click", function (event) {
+        // 기본 이벤트 제거
+        event.preventDefault();
+
+        // 선택된 나이 가져오기
+        const ageSelect = document.getElementById("age-select");
+        const selectedAge = ageSelect.options[ageSelect.selectedIndex].value;
+
+        // 나이 선택 여부 확인
+        if (selectedAge === "") {
+            alert("만 나이를 선택해주세요.");
+            return;
+        }
+
+        //window.location.href = "testPage_local.html"; // 여기 지워야 fetch 가 실행되었음
+        // 서버에 요청하기
+        fetch("http://15.164.169.174:8080/members", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                age: selectedAge
+            })
+        })
+            .then(response => {
+                if (response.ok) {
+                    console.log("나이 정보에 대해 fetch 와 POST 가 실행됨.");
+                    console.log("");
+                    const locationHeader = response.headers.get("Location");
+                    console.log(locationHeader);
+                    if (locationHeader) {
+                        const membersId = locationHeader.split("/").pop();
+                        localStorage.setItem("members_id", membersId);
+                        console.log("memberID 를 받아옴. 이 경우 : ", membersId);
+                        console.log("");
+                        // test 페이지로 전환
+                        window.location.href = "testPage_local.html";
+                    } else {
+                        throw new Error("Location header not found");
+                    }
+                } else {
+                    throw new Error("HTTP error " + response.status);
+                }
+            })
+            .catch(error => console.error(error));
+    });
+});
---
+++ src/local_ver/test_local.js
@@ -0,0 +1,182 @@
+const blocks = document.querySelectorAll('.time_blck'); // 타이머 쪽 변수
+const countdown = document.getElementById('countdown'); // 카운트다운 변수
+let index = 0; // 타이머 쪽 변수
+let intervalId; // 타이머 쪽 변수
+let remainingTime = 15; // 카운트다운 변수
+const image = document.getElementById('image'); // 이미지 dom
+const prgrs_num_ui = document.getElementById('prgrs_num_ui'); // 진행도 숫자 dom 
+let question_ID = 1;
+
+let mediaStream;
+let mediaRecorder;
+let chunks = [];
+
+function startRecording() {
+    startMediaRecorder();
+}
+
+function startMediaRecorder() { // 이 안에 버튼 활성&비활성 있음 !!!!!!!!
+
+    mediaRecorder = new MediaRecorder(mediaStream);
+    mediaRecorder.start();
+    console.log("question_ID : ", question_ID, "번째. 스트림이 초기화 및 재선언.");
+    console.log("question_ID : ", question_ID, "번째. MediaRecorder.start() 활성화.");
+
+    turn_off('start');
+    turn_on('next');
+
+    mediaRecorder.ondataavailable = function (e) {
+        chunks.push(e.data);
+        console.log("question_ID : ", question_ID, "번째. MediaRecorder.ondataavailable 에 의해 chunks 가 e.data 를 push");
+    };
+
+}
+
+function stopRecording() { // 이 안에 버튼 활성&비활성 있음 !!!!!!!!
+    mediaRecorder.stop();
+    console.log("question_ID : ", question_ID, "번째. MediaRecorder.stop() 발생");
+
+    turn_on('start');
+    turn_off('next');
+
+    mediaRecorder.onstop = function () {
+        const blob = new Blob(chunks, { type: 'audio/mp3' });
+        chunks = [];
+
+        let formData = new FormData();
+        let local_mem_Id = JSON.parse(localStorage.getItem("members_id"));
+        console.log("question_ID : ", question_ID, "번째. localstorage 에서 getItem 으로 memberID 가져옴");
+        formData.append("memberId", local_mem_Id); // 어쩔 수 없이 하드 코딩?
+        formData.append("questionId", question_ID);
+        formData.append("audio", blob);
+        console.log("question_ID : ", question_ID, "번째. fetch 할 formdata 와 blob 이 생성된다.");
+        console.log("question_ID : ", question_ID, "번째. memberID: ", local_mem_Id);
+
+        fetch('http://15.164.169.174:8080/answers', {
+            method: "POST",
+            body: formData
+        })
+        console.log("question_ID : ", question_ID, "번째. 도메인으로 POST 가 진행된다.");
+
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        document.body.appendChild(a);
+        a.href = url;
+        a.download = 'recorded_audio.mp3';
+        a.click();
+        console.log("question_ID : ", question_ID, "번째. 음성 다운로드가 진행된다. ");
+
+        URL.revokeObjectURL(url);
+    };
+}
+
+// Ask for microphone permission when the page loads
+document.addEventListener('DOMContentLoaded', function () {
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then(function (stream) {
+            mediaStream = stream;
+        })
+        .catch(function (err) {
+            console.error('Error accessing microphone', err);
+        });
+});
+// 시간 막대, id=bar 의 class=time_blck 들의 transition 시작. 페이지 로드 시 자동 시작.
+function startTransition() {
+    console.log("question_ID : ", question_ID, "번째. 타이머와 진행도 intervalId 변동에 따라 변화 시작.");
+    intervalId = setInterval(() => {
+        if (index < blocks.length) {
+            blocks[index].style.backgroundColor = 'white';
+            index++;
+        }
+        if (remainingTime > 0) {
+            remainingTime--;
+            countdown.innerHTML = remainingTime.toString();
+        }
+        if (remainingTime == 0) {
+            countdown.innerHTML = '0';
+            setTimeout(() => { // 얘도 특성 상 위 코드보다 먼저 실행되서 강제 연장
+                document.getElementById("next").click();
+            }, 500);
+            setTimeout(() => { // alert 는 무조건 위의 2 문장 보다 먼저 실행되기에, 누구보다 느리게 실행되는 setTimeout 에 가둠
+                alert("아쉽지만 시간이 초과되어, 다음 문제로 이동했어요!");
+            }, 1000);
+        }
+    }, 1000);
+}
+
+// 다음 문제, id=next 누르면 시간 막대, id=bar 의  transition 초기화
+function resetTransition() {
+    index = 0;
+    clearInterval(intervalId);
+    for (let i = 0; i < blocks.length; i++) {
+        blocks[i].style.backgroundColor = 'orange';
+    }
+    remainingTime = 15;
+    countdown.innerHTML = remainingTime.toString();
+    console.log("question_ID : ", question_ID, "번째. 타이머, 진행도, 등 초기화 및 IntervalID clear.");
+}
+
+function img_update() {
+    console.log("question_ID : ", question_ID, "번째. 도메인 API 에서 사진을 받아와 img src 를 변경.");
+    fetch(`http://15.164.169.174:8080/questions/${question_ID}`, {
+        method: "GET"
+    })
+        .then(response => response.json())
+        .then(data => {
+            image.src = data.imageUrl;
+        })
+}
+
+function img_hide() {
+    console.log("question_ID : ", question_ID, "번째. 사진을 다시 물음표로 변환해 숨긴다.");
+    image.src = "https://cdn-icons-png.flaticon.com/512/2732/2732700.png";
+}
+
+function turn_off(id) {
+    console.log("question_ID : ", question_ID, "번째. ", id, " 버튼을 disabled 시키고 배경색을 바꾼다.");
+    document.getElementById(id).disabled = true;
+    document.getElementById(id).style.backgroundColor = 'darkslategray';
+}
+
+function turn_on(id) {
+    console.log("question_ID : ", question_ID, "번째. ", id, " 버튼을 활성화시키고 배경색을 원래 가져야 할 색으로.");
+    document.getElementById(id).disabled = false;
+    if (id == 'start') {
+        document.getElementById(id).style.backgroundColor = 'green';
+    } else if (id == 'next') {
+        document.getElementById(id).style.backgroundColor = 'yellow';
+    }
+}
+
+document.getElementById("start").addEventListener('click', () => {
+    console.log("");
+    console.log("===시작 new===", "question_ID : ", question_ID,);
+    console.log("");
+    console.log("question_ID : ", question_ID, "번째. start 버튼 클릭, 검사 시작.");
+
+    startRecording();
+    startTransition();
+    img_update();
+
+    console.log("");
+    console.log("==시작 end==, ", "question_ID : ", question_ID,);
+    console.log("");
+});
+
+document.getElementById("next").addEventListener('click', () => {
+    console.log("");
+    console.log("==종료 new==", "question_ID : ", question_ID);
+    console.log("");
+    console.log("question_ID : ", question_ID, "번째. next 버튼이 눌렸다.");
+
+    stopRecording();
+    resetTransition();
+    img_hide();
+
+    question_ID = question_ID + 1;
+    prgrs_num_ui.innerHTML = question_ID;
+
+    console.log("");
+    console.log("==종료 end==", "question_ID : ", question_ID);
+    console.log("");
+});
