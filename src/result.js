c677cdb77f9577d0a6dde9392b3f4fafcd1c7f6d
hufsyewon <127353816+hufsyewon@users.noreply.github.com>
2023-05-30 07:30:08 +0900

    getDataFromServer를 setTime실행 이후로

--- src/result.js
+++ src/result.js
@@ -1,74 +1,109 @@
-window.addEventListener('DOMContentLoaded', function() {
+function updateTableTwo(results) {
+    const tableOneRows = document.querySelectorAll("#resultTableOne tr");
+    const tableTwoRows = document.querySelectorAll("#resultTableTwo tr");
+  
+    if (tableOneRows && tableOneRows.length > 0 && results && results.length > 0) {
+      for (let i = 1; i < tableTwoRows.length - 1; i++) {
+        const row = tableTwoRows[i];
+  
+        const resultIndex = i + tableOneRows.length - 2;
+  
+        if (resultIndex < results.length) {
+          const isCorrect = results[resultIndex].isCorrect;
+          row.querySelector("td:last-child").textContent = isCorrect;
+          const word = results[resultIndex].word;
+          row.querySelector("td:nth-child(2)").textContent = word;
+        }
+      }
+    } else {
+      console.error('Invalid tableOneRows or results data:', tableOneRows, results);
+    }
+  }
+  
+  // 페이지가 로드될 때 데이터를 가져옵니다.
+  window.addEventListener('DOMContentLoaded', function() {
     const overlay = document.getElementById('loading-overlay');
-
     overlay.style.display = 'flex';
-
+  
     // 데이터 로딩이 완료되면 오버레이를 숨김
-    // 10초 후에 데이터 로딩 완료, 오버레이 숨김
+    // 5초 후에 데이터 로딩 완료, 오버레이 숨김
     setTimeout(function() {
       overlay.style.display = 'none';
-    }, 2000);
+    }, 5000);
+    getDataFromServer();
   });
   
-
-// GET 요청을 보내고 서버에서 응답 데이터를 처리하는 함수
-function getDataFromServer() {
-    // 서버 URL을 설정합니다.
+  // GET 요청을 보내고 서버에서 응답 데이터를 처리하는 함수
+  function getDataFromServer() {
+    //const memberID = 233;
     const memberID = localStorage.getItem('members_id');
-    const serverURL = `https://api.bnt-15.kr/results/${memberID}`; // 실제 서버 URL로 변경해주세요.
-
+    const serverURL = `https://api.bnt-15.kr/results/${memberID}`;
+  
     // GET 요청을 보냅니다.
-    fetch(serverURL)
-        .then(response => response.json())
-        .then(data => {
-            // 응답 데이터를 처리합니다.
-            const averageScore = data.averageScore;
-            const totalScore = data.totalScore;
-            const results = data.results;
-
-
-            // 결과를 HTML에 표시합니다.
-            document.getElementById("averageScore").textContent = `${averageScore}`;
-            document.getElementById("totalscore").textContent = `${totalScore}`;
-            document.getElementById("totalScore").textContent = totalScore;
-
-            // totalScore가 averageScore보다 높은지 낮은지를 표시합니다.
-            let comparisonText;
-            if (totalScore > averageScore) {
-                comparisonText = ' 높습니다.';
-            } else if (totalScore < averageScore) {
-                comparisonText = ' 낮습니다.';
-            } else {
-                comparisonText = ' 평균 점수입니다.';
-            }
-            document.getElementById('comparison').textContent = comparisonText;
-
-            const tableOneRows = document.querySelectorAll("#resultTableOne tr");
-            for (let i = 1; i < tableOneRows.length; i++) {
-                const row = tableOneRows[i];
-                const isCorrect = results[i - 1].isCorrect; // results 배열이 0부터 시작한다고 가정합니다.
-                document.getElementById(i.toString()).textContent = isCorrect;
-                const word = results[i - 1].word; // results 배열이 0부터 시작한다고 가정합니다.
-                row.querySelector("td:nth-child(2)").textContent = word;
-            }
-
-            // resultTableTwo를 업데이트합니다 (한 행의 예시)
-            const tableTwoRows = document.querySelectorAll("#resultTableTwo tr");
-            for (let i = 1; i < tableTwoRows.length - 1; i++) {
-                const row = tableTwoRows[i];
-                const isCorrect = results[i + tableOneRows.length - 2].isCorrect; // results 배열이 0부터 시작한다고 가정합니다.
-                row.querySelector("td:last-child").textContent = isCorrect;
-                const word = results[i + tableOneRows.length - 2].word; // results 배열이 0부터 시작한다고 가정합니다.
-                row.querySelector("td:nth-child(2)").textContent = word;
-
-            }
-            // 나머지 코드는 동일합니다
-        })
-        .catch(error => {
-            // 오류가 발생한 경우 처리합니다.
-            console.error('데이터를 가져오는 중 오류가 발생했습니다:', error);
-        });
-}
-
-// 페이지가 로드될 때 데이터를 가져옵니다.
-getDataFromServer();
+    return fetch(serverURL)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not OK');
+        }
+        return response.json();
+      })
+      .then(data => {
+        handleResponseData(data);
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+        // Handle the error gracefully (e.g., display an error message)
+      });
+  }
+  
+  
+  function handleResponseData(data) {
+    const averageScore = data.averageScore;
+    const totalScore = data.totalScore;
+    const results = data.results;
+  
+    displayScores(averageScore, totalScore);
+    displayComparisonText(averageScore, totalScore);
+    updateTableOne(results);
+    updateTableTwo(results);
+  }
+  
+  function displayScores(averageScore, totalScore) {
+    document.getElementById("averageScore").textContent = averageScore;
+    document.getElementById("totalscore").textContent = totalScore;
+    document.getElementById("totalScore").textContent = totalScore;
+  }
+  
+  function displayComparisonText(averageScore, totalScore) {
+    let comparisonText;
+    if (totalScore > averageScore) {
+      comparisonText = ' 높습니다.';
+    } else if (totalScore < averageScore) {
+      comparisonText = ' 낮습니다.';
+    } else {
+      comparisonText = ' 평균 점수입니다.';
+    }
+    document.getElementById('comparison').textContent = comparisonText;
+  }
+  
+  function updateTableOne(results) {
+    const tableOneRows = document.querySelectorAll("#resultTableOne tr");
+  
+    // Check if results is defined and has the expected structure
+    if (results && Array.isArray(results)) {
+      for (let i = 1; i < tableOneRows.length; i++) {
+        const row = tableOneRows[i];
+  
+        // Check if the index is within the bounds of the results array
+        if (i - 1 < results.length) {
+          const isCorrect = results[i - 1].isCorrect;
+          document.getElementById(i.toString()).textContent = isCorrect;
+          const word = results[i - 1].word;
+          row.querySelector("td:nth-child(2)").textContent = word;
+        }
+      }
+    } else {
+      console.error('Invalid results data:', results);
+    }
+  }
+  
