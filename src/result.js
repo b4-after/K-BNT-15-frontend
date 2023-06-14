cd1155ba9029bc5543ef767debd0673991ff1459
leehjhjhj <102458609+leehjhjhj@users.noreply.github.com>
2023-06-14 16:40:48 +0900

    Update result.js

--- src/result.js
+++ src/result.js
@@ -47,8 +47,8 @@ function updateTableTwo(results) {
   
   // GET 요청을 보내고 서버에서 응답 데이터를 처리하는 함수
   function getDataFromServer() {
-    const memberID = localStorage.getItem('members_id');
-//     const memberID = 477;
+//     const memberID = localStorage.getItem('members_id');
+    const memberID = 477;
     const serverURL = `https://api.bnt-15.kr/results/${memberID}`;
   
     // GET 요청을 보냅니다.
